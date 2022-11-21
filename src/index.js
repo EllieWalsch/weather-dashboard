4a4d4b9542896b4941574007342f684bdcbdb384
EllieWalsch <elliewalsch4@gmail.com>
2022-11-21 17:03:25 -0600

    Add new function for display date

--- src/index.js
+++ src/index.js
@@ -17,13 +17,7 @@ form.addEventListener("submit", async (event) => {
     const cityName = event.target.city.value;
     currentCity.innerText = cityName;
 
-    // Display date
-    const dateEl = document.querySelector("#date");
-    const date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    dateEl.innerText = `${month}/${day}/${year}`;
+    initDate();
 
     let coords = await apiServices.getCoords(cityName);
 
@@ -49,5 +43,14 @@ form.addEventListener("submit", async (event) => {
     console.log(forecast);
 
 });
+
+function initDate() {
+    const dateEl = document.querySelector("#date");
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    dateEl.innerText = `${month}/${day}/${year}`;
+};
 
 
